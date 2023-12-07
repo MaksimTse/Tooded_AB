e6824a1568fe5478a5d9aaa0d6a49c5ff71e818d
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-12-07 09:50:59 +0200

    Add files via upload

---
+++ Admin.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Tooded_AB
+{
+    public partial class Admin : Form
+    {
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\AppData\Tooded.mdf;Integrated Security=True");
+
+        SqlDataAdapter adapter_toode, adapter_kategooria;
+        SqlCommand command;
+
+        SaveFileDialog save;
+        OpenFileDialog open;
+
+        int Id;
+        public Admin()
+        {
+            InitializeComponent();
+            NaitaAndmed();
+            NaitaKategooriad();
+
+            dataGridView1.SelectionChanged += dataGridView1_SelectionChanged;
+        }
+
+        public void NaitaAndmed()
+        {
+            connect.Open();
+            DataTable dt_Toode = new DataTable();
+
+            adapter_toode = new SqlDataAdapter("SELECT T.Id, T.Toodenimetus, T.Kogus, T.Hind, T.Pilt, K.Kategooria_nimetus as Kategooria " +
+                "FROM Toodetable T INNER JOIN Kategooriatable K ON T.Kategooria = K.Id", connect);
+            adapter_toode.Fill(dt_Toode);
+            dataGridView1.Columns.Clear();
+            dataGridView1.DataSource = dt_Toode;
+            DataGridViewComboBoxColumn combo_kat = new DataGridViewComboBoxColumn();
+            combo_kat.HeaderText = "Kategooria";
+            combo_kat.Name = "KategooriaColumn";
+            combo_kat.DataPropertyName = "Kategooria";
+            HashSet<string> uniqueCategories = new HashSet<string>();
+            foreach (DataRow item in dt_Toode.Rows)
+            {
+                string category = item["Kategooria"].ToString();
+                if (!uniqueCategories.Contains(category))
+                {
+                    uniqueCategories.Add(category);
+                    combo_kat.Items.Add(category);
+                }
+            }
+            dataGridView1.Columns.Add(combo_kat);
+            dataGridView1.Columns["Kategooria"].Visible = false;
+                       
+            
+            connect.Close();
+        }
+        private void NaitaKategooriad()
+        {
+            connect.Open();
+            DataTable dt_Kategooria = new DataTable();
+            adapter_kategooria = new SqlDataAdapter("SELECT * FROM Kategooriatable", connect);
+            adapter_kategooria.Fill(dt_Kategooria);
+
+            comboBox1.MaxDropDownItems = dt_Kategooria.Rows.Count;
+
+            foreach (DataRow item in dt_Kategooria.Rows)
+            {
+                comboBox1.Items.Add(item["Kategooria_nimetus"].ToString().Trim());
+            }
+
+            connect.Close();
+        }
+
+
+        private void Lisa_Kat_btn_Click(object sender, EventArgs e)
+        {
+            string newCategory = comboBox1.Text.Trim();
+
+            if (!string.IsNullOrEmpty(newCategory) && !KategooriaExists(newCategory))
+            {
+                try
+                {
+                    connect.Open();
+                    command = new SqlCommand("INSERT INTO Kategooriatable (Kategooria_nimetus) VALUES (@kat)", connect);
+                    command.Parameters.AddWithValue("@kat", newCategory);
+                    command.ExecuteNonQuery();
+                    connect.Close();
+                    comboBox1.Items.Clear();
+                    NaitaKategooriad();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Andmebaasiga viga!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Kategooria on juba olemas või sisestatud andmed on puudulikud!");
+            }
+        }
+
+        private bool KategooriaExists(string categoryName)
+        {
+            connect.Open();
+            SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Kategooriatable WHERE Kategooria_nimetus = @kat", connect);
+            command.Parameters.AddWithValue("@kat", categoryName);
+            int count = (int)command.ExecuteScalar();
+            connect.Close();
+
+            return count > 0;
+        }
+
+        private void Kustuta_Kat_btn_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedItem != null) 
+            { 
+                string val_kat = comboBox1.SelectedItem.ToString();
+
+                connect.Open();
+                SqlCommand command = new SqlCommand("DELETE FROM Kategooriatable WHERE Kategooria_nimetus = @kat", connect);
+                command.Parameters.AddWithValue("@kat", val_kat);
+                command.ExecuteNonQuery();
+                connect.Close();
+                comboBox1.Items.Clear();
+                NaitaKategooriad();
+            }
+
+        }
+
+        private void Lisa_btn_Click(object sender, EventArgs e)
+        {
+            if (Toode_txt.Text.Trim() != string.Empty && Kogus_txt.Text.Trim() != string.Empty && Hind_txt.Text.Trim() != string.Empty && comboBox1.SelectedItem != null)
+            {
+                try
+                {
+                    open = new OpenFileDialog();
+                    open.Multiselect = false;
+                    open.Filter = "Images Files(*.jpeg;*.png;*.jpg;*.bmp)|*.jpeg;*.png;*.jpg;*.bmp";
+
+                    if (open.ShowDialog() == DialogResult.OK)
+                    {
+                        connect.Open();
+
+                        command = new SqlCommand("SELECT Id FROM Kategooriatable WHERE Kategooria_nimetus=@kat", connect);
+                        command.Parameters.AddWithValue("@kat", comboBox1.Text);
+                        command.ExecuteNonQuery();
+                        Id = Convert.ToInt32(command.ExecuteScalar());
+
+                        string imagePath = open.FileName;
+                        string imageName = Path.GetFileName(imagePath);
+
+                        Toode_pb.Image = Image.FromFile(imagePath);
+
+                        command = new SqlCommand("INSERT INTO Toodetable (Toodenimetus, Kogus, Hind, Pilt, Kategooria) VALUES" +
+                        " (@toode, @kogus, @hind, @pilt, @kat)", connect);
+                        command.Parameters.AddWithValue("@toode", Toode_txt.Text);
+                        command.Parameters.AddWithValue("@kogus", Kogus_txt.Text);
+                        command.Parameters.AddWithValue("@hind", Hind_txt.Text);
+                        command.Parameters.AddWithValue("@pilt", imageName);
+                        command.Parameters.AddWithValue("@kat", Id);
+
+                        command.ExecuteNonQuery();
+
+                        connect.Close();
+
+                        NaitaAndmed();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Andmebaasiga viga: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sisesta andmeid!");
+            }
+        }
+
+        private void MuudaToode(int toodeId, string uusToodeNimetus, int uusKogus, decimal uusHind, string uusPilt, int uusKategooriaId)
+        {
+            connect.Open();
+            SqlCommand updateCommand = new SqlCommand("UPDATE Toodetable SET Toodenimetus = @uusToodeNimetus, Kogus = @uusKogus, Hind = @uusHind, Pilt = @uusPilt, Kategooria = @uusKategooria WHERE Id = @toodeId", connect);
+            updateCommand.Parameters.AddWithValue("@uusToodeNimetus", uusToodeNimetus);
+            updateCommand.Parameters.AddWithValue("@uusKogus", uusKogus);
+            updateCommand.Parameters.AddWithValue("@uusHind", uusHind);
+            updateCommand.Parameters.AddWithValue("@uusPilt", uusPilt);
+            updateCommand.Parameters.AddWithValue("@uusKategooria", uusKategooriaId);
+            updateCommand.Parameters.AddWithValue("@toodeId", toodeId);
+            updateCommand.ExecuteNonQuery();
+            connect.Close();
+        }
+
+        private void Muuda_btn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow val_rida = dataGridView1.SelectedRows[0];
+                int val_rida_ID = Convert.ToInt32(val_rida.Cells["Id"].Value);
+
+                if (Toode_txt.Text.Trim() != string.Empty && Kogus_txt.Text.Trim() != string.Empty && Hind_txt.Text.Trim() != string.Empty && comboBox1.SelectedItem != null)
+                {
+                    OpenFileDialog open = new OpenFileDialog();
+                    open.InitialDirectory = @"..\..\Images";
+                    open.Multiselect = false;
+                    open.Filter = "Images Files(*.jpeg;*.png;*.jpg;*.bmp)|*.jpeg;*.png;*.jpg;*.bmp";
+
+                    if (open.ShowDialog() == DialogResult.OK)
+                    {
+                        string imageFileName = Path.GetFileName(open.FileName);
+
+                        string UUS_ToodeNimetus = Toode_txt.Text;
+                        int UUS_Kogus = int.Parse(Kogus_txt.Text);
+                        float UUS_Hind = float.Parse(Hind_txt.Text);
+                        string UUS_Pilt = imageFileName;
+                        string UUS_Kategooria = comboBox1.SelectedItem.ToString();
+
+                        try
+                        {
+                            if (connect.State == ConnectionState.Closed)
+                            {
+                                connect.Open();
+                            }
+                            command = new SqlCommand("SELECT Id FROM Kategooriatable WHERE Kategooria_nimetus = @kat", connect);
+                            command.Parameters.AddWithValue("@kat", UUS_Kategooria);
+                            int kategooriaId = Convert.ToInt32(command.ExecuteScalar());
+
+                            command = new SqlCommand("UPDATE Toodetable SET Toodenimetus = @toode, Kogus = @kog, Hind = @pc, Pilt = @pilt, Kategooria = @katID WHERE Id = @id", connect);
+                            command.Parameters.AddWithValue("@toode", UUS_ToodeNimetus);
+                            command.Parameters.AddWithValue("@kog", UUS_Kogus);
+                            command.Parameters.AddWithValue("@pc", UUS_Hind);
+                            command.Parameters.AddWithValue("@pilt", UUS_Pilt);
+                            command.Parameters.AddWithValue("@katID", kategooriaId);
+                            command.Parameters.AddWithValue("@id", val_rida_ID);
+
+                            command.ExecuteNonQuery();
+                            connect.Close();
+                            NaitaAndmed();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Probleem tekkis uuendamisel: " + ex.Message);
+                        }
+                        finally
+                        {
+                            if (connect.State == ConnectionState.Open)
+                            {
+                                connect.Close();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Vali pilt!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Sisesta andmed!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vali rida DataGridView-l uuendamiseks.");
+            }
+        }
+
+        private void KustutaAndmed(int toodeId)
+        {
+            connect.Open();
+            SqlCommand deleteCommand = new SqlCommand("DELETE FROM Toodetable WHERE Id = @toodeId", connect);
+            deleteCommand.Parameters.AddWithValue("@toodeId", toodeId);
+            deleteCommand.ExecuteNonQuery();
+            connect.Close();
+        }
+
+        private void Kustuta_btn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int selectedRowIndex = dataGridView1.SelectedRows[0].Index;
+                int selectedToodeId = Convert.ToInt32(dataGridView1.Rows[selectedRowIndex].Cells["Id"].Value);
+
+                KustutaAndmed(selectedToodeId);
+
+                NaitaAndmed();
+            }
+            else
+            {
+                MessageBox.Show("Valige rida, mida soovite kustutada!");
+            }
+        }
+        private void otsi_fail_btn_Click(object sender, EventArgs e)
+        {
+            open = new OpenFileDialog();
+            open.InitialDirectory = @"C:\Users\opilane\source\repos\TARpv22_Maksim_Tsepelevits\Tooded_AB\bin\Debug";
+            open.Multiselect = true;
+            open.Filter = "Images Files(*.jpeg;*.png;*.jpg;*.bmp)|*jpeg;*.png;*.jpg;*.bmp";
+
+
+            FileInfo open_info = new FileInfo(@"C:\Users\opilane\source\repos\TARpv22_Maksim_Tsepelevits\Tooded_AB\bin\Debug\" + open.FileName);
+            if (open.ShowDialog()==DialogResult.OK && Toode_txt.Text != null)
+            {
+                save = new SaveFileDialog();
+                save.InitialDirectory = Path.GetFullPath(@"..\..\Images");
+                save.FileName = Toode_txt.Text + Path.GetExtension(open.FileName);
+                save.Filter = "Images" + Path.GetExtension(open.FileName)+"|"+Path.GetExtension(open.FileName);
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    File.Copy(open.FileName, save.FileName);
+                    Image img = Image.FromFile(save.FileName);
+                    Toode_pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                    Toode_pb.ClientSize = new Size(200, 200);
+                    Toode_pb.Image = (Image)(new Bitmap(img, Toode_pb.ClientSize));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Puudub toode nimetus või oli vajutatud Cancel");
+            }
+
+            open.ShowDialog();
+        }
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                string imageName = dataGridView1.SelectedRows[0].Cells["Pilt"].Value.ToString();
+                string imagePath = Path.Combine(Path.GetFullPath(@"..\..\Images"), imageName);
+
+                if (File.Exists(imagePath))
+                {
+                    Image img = Image.FromFile(imagePath);
+
+                    Toode_pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                    Toode_pb.ClientSize = new Size(200, 200);
+                    Toode_pb.Image = (Image)(new Bitmap(img, Toode_pb.ClientSize));
+                }
+                else
+                {
+                    MessageBox.Show($"Image file '{imageName}' not found.");
+                }
+            }
+            else
+            {
+                Toode_pb.Image = null;
+            }
+        }
+
+
+    }
+}
---
+++ Login.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Tooded_AB
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.PassLogin = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(270, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(267, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Logi sisse";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(446, 324);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Loo uus konto";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(270, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // PassLogin
+            // 
+            this.PassLogin.Location = new System.Drawing.Point(270, 197);
+            this.PassLogin.Name = "PassLogin";
+            this.PassLogin.Size = new System.Drawing.Size(267, 20);
+            this.PassLogin.TabIndex = 4;
+            this.PassLogin.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(280, 335);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 23);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Teil ei ole kontot?";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(266, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 23);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Salasõna";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(266, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 23);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Kasutaja nimi";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(378, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 23);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Unustanud salasõna?";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.PassLogin);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Login";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox PassLogin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
